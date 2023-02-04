641cde11ffaed99e73433abf07f5a98925a04377
ughhlynx <contato.daniellegsantos@gmail.com>
2023-02-03 23:51:36 -0300

    js atualização

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
 const jump = () => {
     mario.classList.add('jump');
+    setTimeout(()=> {
+        mario.classList.remove('jump');
+    }, 500);
 }
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = mario.offset
+    console.log(pipePosition)
+    if (pipePosition <= 120) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px` 
+    }
+}, 10)
+
 document.addEventListener('keydown', jump);
